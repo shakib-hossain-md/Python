090332040a5096d698c7dee8b4f92aa566deb70c
shakib-hossain-md <sakibsanto01@gmail.com>
2024-10-03 01:56:23 +0600

    Upload

---
+++ Access list items.py
@@ -0,0 +1,26 @@
+#Print the second item of the list
+
+thislist = ["apple","banana","cherry"]
+print(thislist[1])
+
+#Negative indexing
+
+#print the last item of the list
+
+thislist = ["apple","banana","cherry"]
+print(thislist[-1])
+
+#Range of indexes
+
+#Return 3rd, 4th and 5th item
+
+thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(thislist[2:5])
+
+
+#Check if item exist
+
+thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list.")
+  
