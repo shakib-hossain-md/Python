1ed1eeebc7eabe73e18983e9b271297b18390e21
shakib-hossain-md <sakibsanto01@gmail.com>
2024-10-03 18:56:56 +0600

    Upload

---
+++ List comprehension.py
@@ -0,0 +1,15 @@
+#Without list comprehension
+
+fruits = ["apple","banana","cherry","kiwi","mango"]
+newlist = []
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+print(newlist)
+
+#With list comprehension
+
+fruits = ["apple","banana","cherry","kiwi","mango"]
+newlist = [x for x in fruits if "a" in x]
+print(newlist)
+
