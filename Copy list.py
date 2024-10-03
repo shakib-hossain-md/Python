0ee38f68b79e7fcf60381e09e5582721e7e4a868
shakib-hossain-md <sakibsanto01@gmail.com>
2024-10-04 00:50:05 +0600

    Upload

---
+++ Copy list.py
@@ -0,0 +1,17 @@
+#Use the copy() method
+
+thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+mylist = thislist.copy()
+print(mylist)
+
+#Use the list() method
+
+thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+mylist = list(thislist)
+print(mylist)
+
+#Use the slice operator
+
+thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+mylist = thislist[:]
+print(mylist)
