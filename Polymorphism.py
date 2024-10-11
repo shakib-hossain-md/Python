ed5158bff3c903e42621c0f8504b74b287008efb
shakib-hossain-md <sakibsanto01@gmail.com>
2024-10-12 00:23:48 +0600

    Upload

---
+++ Polymorphism.py
@@ -0,0 +1,59 @@
+# Polymorphysm means "many forms"
+
+# Class polymorphism
+class Car:
+  def __init__(self,brand,model):
+    self.brand = brand
+    self.model = model
+  def move(self):
+    print("Drive")
+
+class Boat:
+  def __init__(self,brand,model):
+    self.brand = brand
+    self.model = model
+  def move(self):
+    print("Sail")
+
+class Plane:
+  def __init__(self,brand,model):
+    self.brand = brand
+    self.model = model
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford","Mustang")
+boat1 = Boat("Ibiza","Touring 20")
+plane1 = Plane("Boeing","747")
+
+for x in (car1,boat1,plane1):
+  x.move()
+
+
+
+# Inheritance class polymorphism
+class Vehicle:
+  def __init__(self,brand,model):
+    self.brand = brand
+    self.model = model
+  def move(self):
+    print("Move")
+
+class Car(Vehicle):
+  pass
+class Boat(Vehicle):
+  def move(self):
+    print("Sail")
+
+class Plane(Vehicle):
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford","Mustang")
+boat1 = Boat("Ibiza","Touring 20")
+plane1 = Plane("Boeing","747")
+
+for x in (car1,boat1,plane1):
+  print(x.brand)
+  print(x.model)
+  x.move()
